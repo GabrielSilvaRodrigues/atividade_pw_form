2105a727164b43aa38c8c54cb11a4fbcc8fd9864
Gabriel Rodrigues <169057592+GabrielSilvaRodrigues@users.noreply.github.com>
2025-04-14 10:46:34 -0300

    update

--- database/banco_dados.sql
+++ database/banco_dados.sql
@@ -1,9 +1,35 @@
 CREATE TABLE usuario (
-	id_usuario INTEGER PRIMARY KEY autoincrement,
+	id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
 	nm_usuario VARCHAR(100),
-	email VARCHAR(150),
-	senha VARCHAR(30),
-	cpf CHAR(14)
+	email_usuario VARCHAR(150),
+	telefone_usuario CHAR(15),
+	data_nascimento_usuario DATE,
+	senha_usuario VARCHAR(30),
+	cpf_usuario CHAR(14),
+	genero_usuario CHAR(1),
+	mensagem_usuario VARCHAR(200)
+);
+CREATE TABLE profissional(
+	id_profissional INTEGER PRIMARY KEY AUTOINCREMENT,
+	nm_profissional VARCHAR(100),
+	email_profissional VARCHAR(150),
+	telefone_profissional CHAR(15),
+	data_nascimento_profissional DATE,
+	senha_profissional VARCHAR(30),
+	cpf_profissional CHAR(14),
+	genero_profissional CHAR(1),
+	mensagem_profissional VARCHAR(200)
+);
+CREATE TABLE empresa(
+	id_empresa INTEGER PRIMARY KEY AUTOINCREMENT,
+	nm_empresa VARCHAR(100),
+	email_empresa VARCHAR(150),
+	telefone_empresa CHAR(15),
+	senha_empresa VARCHAR(30),
+	cnpj CHAR(18),
+	mensagem_empresa VARCHAR(200)
 );
 
 DROP TABLE usuario;
+DROP TABLE profissional;
+DROP TABLE empresa;
